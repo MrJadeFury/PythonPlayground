7309852467278f420fe97b2d7d5da97f90e8e6ad
Jadefen <38327135+MrJadeFury@users.noreply.github.com>
2019-04-15 02:02:07 -0700

    trying out stuff

--- launchlovelivesoldier.py
+++ launchlovelivesoldier.py
@@ -1,25 +1,4 @@
-#Variables
-soldiergamelink = 'https://www.youtube.com/watch?v=6_YUxyhwk2I'
+from pywinauto import application
 
-#imports
-import webbrowser
-import time
-import pyautogui
-import pywinauto
-
-#Open Never Gonna Give You Up
-webbrowser.open(soldiergamelink)
-
-#Increase to Max Volume
-for x in range(0, 60):
-    pyautogui.press('volumeup')
-
-#Wait 1 Second
-time.sleep(5)
-
-#Make It Full Screen
-pyautogui.press('F')
-
-#Finish Program
-print("Program Complete. GET RICK ROLLED KEKEKEK!")
-
+app = application.Application()
+app.start("ffxivboot.exe")
