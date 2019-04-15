d5c32761abdbd004a399e5d7a40cdc2c037ab7d3
Jadefen <38327135+MrJadeFury@users.noreply.github.com>
2019-04-15 00:19:44 -0700

    started rick roll script

---
+++ rickroll.py
@@ -0,0 +1,12 @@
+#Variables
+rickrolllink = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+
+#Open Never Gonna Give You Up
+import webbrowser
+webbrowser.open(rickrolllink)
+
+#Wait 1 Second
+import time
+time.sleep(1)
+
+#Make It Full Screen
